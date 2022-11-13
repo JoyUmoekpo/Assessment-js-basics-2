96a6762a5e862c9eaa4c9070cae6eeb2910bf408
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-11-12 18:55:49 -0600

    Problem 3 Solved

--- cart.js
+++ cart.js
@@ -88,6 +88,7 @@ console.log('---------------------------------------------------------');
 /*
     TEXT ANSWER HERE
 
+    My customer object will have the following properties: name, phoneNumber, email, address, order, quantity, totalAmount, paymentType, and cardNumber. A restaurant would need this information to know who made the order and all the relevant information for the customer to pay for their order and have the food delivered to the correct address. The phoneNumber, quantity, totalAmount, and cardNumber are all number data types because they are the properties containing only numbers. The totalAmount property could also be classified as a floating point number due to the presence of a decimal. The name, email, address, and 'order' properties are all string data types because they are contain in quote marks and are a mix of letter characters and numbers when applicable. Lastly, paymentType is an array because the customer will have the choice to choose how they will pay for their order and will put in a card number if the customer chooses to pay with a debit or credit card.
 */
 
 /*
@@ -96,3 +97,17 @@ console.log('---------------------------------------------------------');
 */
 
 //CODE HERE
+
+let customer = {
+    name: 'Noé Arch',
+    phoneNumber: 1234567890,
+    email: 'noearch@gmail.com',
+    address:'987 French Street, French City, 47947',
+    order: 'Large Cheese Pizza, Extra Sauce, Sprite',
+    quantity: 1,
+    totalAmount: 20.78,
+    paymentType: ['cash', 'credit card', 'debit card'],
+    cardNumber: 0987656321
+}
+
+console.log(customer);
